5cacc1c11132b71483d84126fddc033eaaf1c849
abhisheksirauthiya2004 <133157838+abhisheksirauthiya2004@users.noreply.github.com>
2025-05-30 15:43:36 +0000

    container config

---
+++ script2.js
@@ -0,0 +1,19 @@
+console.log("start of script 2");
+
+// setTimeout(() =>{
+//     console.log("this is from the task Queue (setTimeout)");
+// },0);
+setTimeout(() =>{
+    console.log("A")
+},0);
+
+setTimeout(() =>{
+    console.log("B")
+},0);
+
+setTimeout(() =>{
+    console.log("C")
+},0);
+
+console.log("end of script"); 
+console.log("bye bye");
